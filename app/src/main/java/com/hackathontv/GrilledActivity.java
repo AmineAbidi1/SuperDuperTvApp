0b08c42cd47406d4bbad42e08349ae18e7ce4000
Jacek Krasowski <jacekkrasowski@gmail.com>
2016-04-29 16:27:09 +0200

    added grilled image for sharing (without sharing)

---
+++ app/src/main/java/com/hackathontv/GrilledActivity.java
@@ -0,0 +1,13 @@
+package com.hackathontv;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GrilledActivity extends Activity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.grilled);
+    }
+}
